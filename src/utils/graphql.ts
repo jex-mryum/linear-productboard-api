10be6482a82d60c69d080a3ee85dcbe9c0474120
Jex Fatin <jex@mryum.com>
2022-03-16 13:02:43 +1100

    Add OpenAPI doc / eg code from graphql-request

--- src/utils/graphql.ts
+++ src/utils/graphql.ts
@@ -8,3 +8,31 @@ const query = gql`
 `;
 
 request(env.productboardApiBaseUrl, query).then(data => console.log(data));
+
+// import { GraphQLClient, gql } from 'graphql-request'
+
+// async function main() {
+//   const endpoint = 'https://api.graph.cool/simple/v1/cixos23120m0n0173veiiwrjr'
+
+//   const graphQLClient = new GraphQLClient(endpoint, {
+//     headers: {
+//       authorization: 'Bearer MY_TOKEN',
+//     },
+//   })
+
+//   const query = gql`
+//     {
+//       Movie(title: "Inception") {
+//         releaseDate
+//         actors {
+//           name
+//         }
+//       }
+//     }
+//   `
+
+//   const data = await graphQLClient.request(query)
+//   console.log(JSON.stringify(data, undefined, 2))
+// }
+
+// main().catch((error) => console.error(error))
