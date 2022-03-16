a5e4ff505b7b1214a21099324298cabccef0c5bb
Jex Fatin <jex@mryum.com>
2022-03-16 17:03:32 +1100

    Remove validation to scope other forms of webhook bodies

--- src/pages/api/linear/update.ts
+++ src/pages/api/linear/update.ts
@@ -1,41 +1,45 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import * as z from 'zod';
 import { env } from '../../../utils/environment';
+import { productboardApiTest } from '../../../utils/graphql';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const { body, method } = req;
   const { action, data } = body;
 
-  const parsed = z
-    .object({
-      action: z.enum([`update`]),
-      data: z.object({
-        id: z.string(),
-        title: z.string(),
-        description: z.string(),
-      }),
-      method: z.string().nonempty(),
-    })
-    .safeParse({
-      action,
-      data,
-      method,
-    });
+  // const parsed = z
+  //   .object({
+  //     action: z.enum([`update`]),
+  //     data: z.object({
+  //       id: z.string(),
+  //       title: z.string(),
+  //       description: z.string(),
+  //     }),
+  //     method: z.string().nonempty(),
+  //   })
+  //   .safeParse({
+  //     action,
+  //     data,
+  //     method,
+  //   });
 
-  if (parsed.success && parsed.data.method != `POST`) {
-    res.status(400).send({ message: `Invalid request method` });
-    return;
-  }
+  // if (parsed.success && parsed.data.method != `POST`) {
+  //   res.status(403).send({ message: `Invalid request method` });
+  //   return;
+  // }
 
-  if (!parsed.success) {
-    console.log(`\n\n${parsed.error}`);
-    res.status(400).send({
-      message: `Invalid request shape or action - please compare the Linear Webhook documentation to the OpenAPI doc for this project`,
-    });
-    return;
-  }
+  // if (!parsed.success) {
+  //   console.log(`\n\n${parsed.error}`);
+  //   res.status(400).send({
+  //     message: `Invalid request shape or action - please compare the Linear Webhook documentation to the OpenAPI doc for this project`,
+  //   });
+  //   return;
+  // }
 
-  console.log(parsed);
+  console.log(body);
+
+  const productboard = await productboardApiTest();
+  console.log(productboard);
 
   res.status(200).send({});
 };
